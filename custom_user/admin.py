9995784f3845e20c7020e870b7743871e1675a8d
Cory Coward <cory@c2softwaresolutions.com>
2023-04-06 00:13:27 -0400

    Add fields to custom user Add signals.py to custom user app Configure custom user admin page

--- custom_user/admin.py
+++ custom_user/admin.py
@@ -9,7 +9,19 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ('username', 'email', 'first_name', 'last_name', )
+    list_display = ('email', 'username', 'first_name', 'last_name', 'is_active', )
+    list_editable = ('is_active', )
+    list_filter = ('is_active', )
+    list_per_page = 25
+    fieldsets = (
+        ('Account Information', {'fields': ('username', 'email', 'first_name', 'last_name', )}),
+        ('Contact Information', {'fields': ('street_address', 'street_address_2', 'city', 'state', 'zip_code',
+                                            'phone', )}),
+        ('Personal and Social Information', {'fields': ('birthday', 'biography', 'facebook', 'twitter', 'instagram',
+                                                        'linkedin', )}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions', )}),
+        ('Account Login Information', {'fields': ('last_login', 'date_joined', )}),
+    )
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- custom_user/models.py
+++ custom_user/models.py
@@ -1,8 +1,24 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils import timezone
 
 
 class CustomUser(AbstractUser):
-    pass
+    street_address = models.CharField(max_length=100)
+    street_address_2 = models.CharField(max_length=100, blank=True)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=25)
+    zip_code = models.CharField(max_length=15)
+    phone = models.CharField(max_length=25)
+    birthday = models.DateField(default=timezone.now)
+    biography = models.TextField()
+    facebook = models.CharField(max_length=250, blank=True)
+    twitter = models.CharField(max_length=250, blank=True)
+    instagram = models.CharField(max_length=250, blank=True)
+    linkedin = models.CharField(max_length=250, blank=True)
 
     def __str__(self):
-        return self.username
+        if self.first_name and self.last_name:
+            return f'{self.first_name} {self.last_name}'
+        else:
+            return self.username
